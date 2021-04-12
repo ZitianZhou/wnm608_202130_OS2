cbd77df57b53ed4df795a9226e92d0a05871113d
𝐙𝐢𝐭𝐢𝐚𝐧 𝐙𝐡𝐨𝐮 <eimit@s-MacBook-Pro.local>
2021-04-12 16:36:46 -0700

    php

---
+++ Zitian.Zhou/parts/meta.php
@@ -0,0 +1,6 @@
+<base href="aau/wnm/Zitian.Zhou/">
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+<script src="https://code.jquery.com/jquery-"></script>
